edbf168e4d84781b6a58f455a39076f4ba7be2ae
Vibe_Coder <ivanlis2003@gmail.com>
2023-07-17 17:30:45 +0300

    0.2.0

---
+++ app.js
@@ -0,0 +1,53 @@
+// to top
+let btnToTop = document.querySelector(".arrow-button");
+let header = document.querySelector("header");
+btnToTop.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+})
+if (header.getBoundingClientRect().bottom < 0) {
+    btnToTop.classList.add("arrow-visible");
+}
+window.addEventListener("scroll", function() {
+  if (header.getBoundingClientRect().bottom < 0) {
+    btnToTop.classList.add("arrow-visible");
+  } else {
+    btnToTop.classList.remove("arrow-visible");
+  }
+})
+
+// slider
+let language = false;
+let slider = document.querySelector(".slider");
+let point = document.querySelector(".point");
+let sliderTextUA = document.querySelector(".slider-text-ua");
+let sliderTextEN = document.querySelector(".slider-text-en");
+slider.addEventListener("click", function() {
+  language = !language;
+  point.classList.toggle("slider-active");
+  point.classList.toggle("slider-inactive");
+  sliderTextUA.classList.toggle("point-invisible");
+  sliderTextEN.classList.toggle("point-invisible");
+  console.log(language)
+})
+
+// text changer
+let textArray = document.querySelectorAll(".text-changer");
+let content;
+fetch("content.json")
+  .then(res => res.json())
+  .then(text => content = text)
+console.log(content);
+
+
+// to contacts
+let btnToContacts = document.querySelector("button");
+let footer = document.querySelector("footer");
+btnToContacts.addEventListener("click", function() {
+  window.scrollTo({
+    top: footer.getBoundingClientRect().bottom,
+    behavior: "smooth"
+  });
+})
