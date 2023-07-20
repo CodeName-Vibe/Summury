07d44f7e722b94ae2f58d1dc371b8b33d1db58d8
Vibe_Coder <ivanlis2003@gmail.com>
2023-07-20 23:53:42 +0300

    0.2.2

--- app.js
+++ app.js
@@ -30,15 +30,128 @@ slider.addEventListener("click", function() {
   point.classList.toggle("slider-inactive");
   sliderTextUA.classList.toggle("point-invisible");
   sliderTextEN.classList.toggle("point-invisible");
-  console.log(language)
 })
 
 // text changer
 let textArray = document.querySelectorAll(".text-changer");
-fetch("content.json")
-  .then(res => res.json())
-  .then(text => console.log(text))
+let content = [
+  [
+    "Name:",
+    "Birthday:",
+    "Lisetskyi Ivan",
+    "24 February 2003",
+    "Contacts",
 
+    "Technologies Stack",
+    "Experienced",
+    "Experienced",
+    "Experienced",
+    "Familiar",
+    "Experienced",
+    "Experienced",
+    "Familiar",
+    "Familiar",
+
+    "Skills",
+    "Fast Learning & Fast Typing",
+    "Reliable & Flexible",
+    "Troubleshooting",
+    "Communicative",
+
+    "Languages",
+    "Ukrainian -",
+    "English -",
+    "Native",
+
+    "Educations",
+    "\"Athens\" Private school",
+    "High",
+    "English B2",
+    "Basic Programming",
+    "SUT",
+    "Computer science",
+    "University",
+    "Self Education",
+    "always",
+
+    "Experience",
+    "VISIT",
+    "University Practise",
+    "1 month",
+    "NDA Project",
+    "2 months",
+    "UNRELEASED",
+
+    "Contacts",
+    "LinkedIn account",
+    "GitHub account",
+    "Djinni account"
+  ],
+  [
+    "Ім'я:",
+    "Д/Н:",
+    "Лісецький Іван",
+    "24 Лютого 2003",
+    "Контакти",
+
+    "Технічний Стек",
+    "Досвідчений",
+    "Досвідчений",
+    "Досвідчений",
+    "Знайомий",
+    "Досвідчений",
+    "Досвідчений",
+    "Знайомий",
+    "Знайомий",
+
+    "Скіли",
+    "Швидко Навчаюсь & Друкую",
+    "Надійний & Гнучкий",
+    "Траблшутинг",
+    "Комунікабельний",
+
+    "Мови",
+    "Українська -",
+    "Англійська -",
+    "Рідна",
+
+    "Навчання",
+    "Приватна школа \"Афіни\"",
+    "Вища",
+    "Англійська B2",
+    "Базове Програмування",
+    "ДУТ",
+    "Комп'ютерні науки",
+    "Університет",
+    "Самоосвіта",
+    "постійно",
+    
+    "Досвід",
+    "ВІДВІДАТИ",
+    "Універ. Практика",
+    "1 місяць",
+    "Проект з ДПН",
+    "2 місяці",
+    "НЕВИПУЩЕНИЙ",
+    
+    "Контакти",
+    "акаунт LinkedIn",
+    "акаунт GitHub",
+    "акаунт Djinni"
+  ]
+] 
+
+slider.addEventListener("click", function() {
+  if (language) {
+    for (let i = 0; i <= textArray.length - 1; i++) {
+      textArray[i].innerHTML = content[1][i];
+    }
+  } else {
+    for (let i = 0; i <= textArray.length - 1; i++) {
+      textArray[i].innerHTML = content[0][i];
+    }
+  }
+})
 
 // to contacts
 let btnToContacts = document.querySelector("button");
